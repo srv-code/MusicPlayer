8109372b5a5003e41d9f54922639f4e3da6d6271
Sourav Dey <sourav.blackjack@gmail.com>
2021-06-20 01:05:16 +0530

    Added header to playback screen

--- src/screens/playback/index.js
+++ src/screens/playback/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ScreenContainer from '../../components/screen-container';
+import screenNames from '../../constants/screen-names';
 
 const Playback = ({ navigation }) => {
   return (
-    <ScreenContainer>
+    <ScreenContainer
+      onBackPress={navigation.goBack}
+      showHeader
+      title={screenNames.playback}
+      subtitle="<Playback track info>">
       <View style={styles.container}>
         <Text>Playback screen</Text>
       </View>
