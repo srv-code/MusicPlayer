b2dc6f47e3834a45755095402ed6147ed61f1e82
Sourav Dey <sourav.blackjack@gmail.com>
2021-06-20 00:55:22 +0530

    Added vector icon support

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useMemo, useState } from 'react';
 import { useColorScheme } from 'react-native';
 import { AppContext } from './src/context/app';
 import Navigator from './src/navigator';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 const App = () => {
   const [enabledDarkTheme, setEnabledDarkTheme] = useState(
@@ -16,9 +17,11 @@ const App = () => {
   }, [enabledDarkTheme]);
 
   return (
-    <AppContext.Provider value={appContext}>
-      <Navigator />
-    </AppContext.Provider>
+    <PaperProvider>
+      <AppContext.Provider value={appContext}>
+        <Navigator />
+      </AppContext.Provider>
+    </PaperProvider>
   );
 };
 
