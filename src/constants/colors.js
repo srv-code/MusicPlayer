a02adf0023f66c598a0a6ed6d28be630619e4086
Sourav Dey <sourav.blackjack@gmail.com>
2021-06-20 21:53:38 +0530

    Made splash screen responsive

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -3,6 +3,7 @@ const colors = {
   darkBlue: '#00004e',
   darkBlue2: '#00009d',
   lightGrey: '#009387',
+  lightGrey1: '#cfcfcf',
   lightBlack: 'rgba(0, 0, 0, .32)',
 };
 export default colors;
--- src/screens/splash/index.js
+++ src/screens/splash/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { View, Text, Dimensions, StyleSheet, StatusBar } from 'react-native';
 import * as Animatable from 'react-native-animatable';
-import LinearGradient from 'react-native-linear-gradient';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import { useTheme } from '@react-navigation/native';
 import appColors from '../../constants/colors';
 import Icons from '../../constants/icons';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 
 const { height } = Dimensions.get('screen');
-const height_logo = height * 0.28;
+const LOGO_HEIGHT = height * 0.28;
+const LOGO_ANIMATION_DURATION = 1500;
 
 const Splash = () => {
-  const { colors } = useTheme();
-
   return (
     <View style={styles.container}>
       <StatusBar
@@ -22,38 +22,15 @@ const Splash = () => {
       <View style={styles.header}>
         <Animatable.Image
           animation="bounceIn"
-          duraton="1500"
+          duraton={LOGO_ANIMATION_DURATION}
           source={Icons.Logo}
           style={styles.logo}
           resizeMode="stretch"
         />
       </View>
-      <Animatable.View
-        style={[
-          styles.footer,
-          // {
-          //   backgroundColor: colors.background,
-          // },
-        ]}
-        animation="fadeInUpBig">
-        <Text
-          style={[
-            styles.title,
-            {
-              color: colors.text,
-              textAlign: 'center',
-              lineHeight: 50,
-            },
-          ]}>
-          Welcome to the World of Music 🎵
-        </Text>
+      <Animatable.View style={styles.footer} animation="fadeInUpBig">
+        <Text style={styles.title}>Welcome to the World of Music 🎵</Text>
         <Text style={styles.text}>Your own very personalized music app ❤</Text>
-        {/*<View style={styles.button}>*/}
-        {/*  <LinearGradient colors={['#08d4c4', '#01ab9d']} style={styles.signIn}>*/}
-        {/*    <Text style={styles.textSign}>Get Started</Text>*/}
-        {/*    <MaterialIcons name="navigate-next" color="#fff" size={20} />*/}
-        {/*  </LinearGradient>*/}
-        {/*</View>*/}
       </Animatable.View>
     </View>
   );
@@ -71,42 +48,26 @@ const styles = StyleSheet.create({
   },
   footer: {
     flex: 1,
-    // backgroundColor: appColors.white,
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-    paddingVertical: 50,
-    paddingHorizontal: 30,
+    paddingVertical: hp(10),
+    paddingHorizontal: hp(10),
   },
   logo: {
-    width: height_logo,
-    height: height_logo,
+    height: LOGO_HEIGHT,
+    width: LOGO_HEIGHT,
   },
   title: {
-    color: '#05375a',
-    fontSize: 30,
+    color: appColors.darkBlue,
+    fontSize: wp(10),
     fontWeight: 'bold',
+    textAlign: 'center',
+    lineHeight: hp(6),
   },
   text: {
-    color: '#cfcfcf',
+    color: appColors.lightGrey1,
     textAlign: 'center',
-    fontSize: 17,
-    marginTop: 20,
-  },
-  button: {
-    alignItems: 'flex-end',
-    marginTop: 30,
-  },
-  signIn: {
-    width: 150,
-    height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 50,
-    flexDirection: 'row',
-  },
-  textSign: {
-    color: 'white',
-    fontWeight: 'bold',
+    fontSize: wp(5),
+    marginTop: hp(3),
+    lineHeight: hp(3),
   },
 });
 
