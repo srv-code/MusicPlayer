2000a685c44a91bb72d5c6431de009e3f5ba54d5
Sourav Dey <sourav.blackjack@gmail.com>
2021-06-21 23:34:34 +0530

    Getting access and trying to fetch all musics in tracks screen

--- src/screens/tracks/index.js
+++ src/screens/tracks/index.js
@@ -1,15 +1,70 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, PermissionsAndroid, StyleSheet, View } from 'react-native';
 import ScreenContainer from '../../components/screen-container';
 import screenNames from '../../constants/screen-names';
 import colors from '../../constants/colors';
-import { Text } from 'react-native-paper';
+import { Button, Text } from 'react-native-paper';
+import MusicFiles from 'react-native-get-music-files';
 
 const Tracks = ({ navigation }) => {
   const [searchedTerm, setSearchedTerm] = useState('');
   const onSearchHandler = term => {
     setSearchedTerm(term);
     console.log(`Searched ${term}`);
+  };
+
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        {
+          title: 'Need permission',
+          message: 'Need storage read permission',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log('You can use the camera');
+      } else {
+        console.log('Camera permission denied');
+      }
+    } catch (err) {
+      Alert.alert(
+        'Permission Error',
+        'Error getting user permission: ' + err.message,
+      );
+    }
+  };
+
+  const fetchAllMusicTracks = () => {
+    requestCameraPermission().then(res => {
+      MusicFiles.getAll({
+        blured: true, // works only when 'cover' is set to true
+        artist: true,
+        duration: true, //default : true
+        cover: true, //default : true,
+        genre: true,
+        title: true,
+        minimumSongDuration: 10000, // get songs bigger than 10000 miliseconds duration,
+        fields: [
+          'title',
+          'albumTitle',
+          'genre',
+          'lyrics',
+          'artwork',
+          'duration',
+        ], // for iOs Version
+      })
+        .then(tracks => {
+          console.log('[fetchAllMusicTracks] response:', tracks);
+        })
+        .catch(error => {
+          console.log('[fetchAllMusicTracks] error:', error);
+          Alert.alert('Error', 'While fetching all tracks: ' + error.message);
+        });
+    });
   };
 
   return (
@@ -23,6 +78,9 @@ const Tracks = ({ navigation }) => {
       onPressSettings={navigation.push.bind(this, screenNames.settings)}>
       <View style={styles.container}>
         <Text>Tracks screen</Text>
+        <Button mode="outlined" onPress={fetchAllMusicTracks}>
+          Fetch tracks
+        </Button>
       </View>
     </ScreenContainer>
   );
