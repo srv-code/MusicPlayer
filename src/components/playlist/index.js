199c126b8c75cd98c2a16d5939a65557cf521917
Sourav Dey <sourav.blackjack@gmail.com>
2021-10-03 13:23:41 +0530

    (Dummy) Implemented rowTranslateAnimatedValues dynamically

--- src/components/playlist/index.js
+++ src/components/playlist/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Animated,
   StyleSheet,
@@ -17,12 +17,12 @@ import { SwipeListView, SwipeRow } from 'react-native-swipe-list-view';
 import Icon from '../icon';
 
 ///////// Test ////////
-const rowTranslateAnimatedValues = {};
-Array(20)
-  .fill('')
-  .forEach((_, i) => {
-    rowTranslateAnimatedValues[`${i}`] = new Animated.Value(1);
-  });
+// const rowTranslateAnimatedValues = {};
+// Array(20)
+//   .fill('')
+//   .forEach((_, i) => {
+//     rowTranslateAnimatedValues[`${i}`] = new Animated.Value(1);
+//   });
 ///////// Test ////////
 
 const rearrangeActions = {
@@ -39,349 +39,27 @@ const rearrangeActions = {
 //  - Proper intial swiping previews
 //  - Long press to select many tracks to re-order or delete them
 const Playlist = ({ style, tracks, setTracks }) => {
-  const [draggingTrackIndex, setDraggingTrackIndex] = useState(null);
-
-  // console.log(`playlist tracks=${JSON.stringify(tracks)}`);
-
-  const renderTrackItem_SwipingPreview = ({
-    item: track,
-    index,
-    drag,
-    isActive,
-  }) => {
-    return (
-      <SwipeRow
-        preview
-        // directionalDistanceChangeThreshold={80}
-        // friction={10}
-        // tension={100}
-        leftActivationValue={wp(10)}
-        leftOpenValue={wp(10)}
-        rightOpenValue={-wp(30)}
-        rightActivationValue={-wp(10)}>
-        <View style={{ backgroundColor: 'blue', height: hp(5) }}>
-          <Text>BACKGROUND</Text>
-        </View>
-        <View style={{ backgroundColor: 'red', height: hp(5) }}>
-          <Text>{track.title}</Text>
-        </View>
-      </SwipeRow>
-    );
-  };
-
-  const renderTrackItem_Method2 = ({ item: track, index, drag, isActive }) => {
-    return (
-      <SwipeListView
-        data={[track]}
-        renderItem={(rowData, rowMap) => {
-          console.log(`rowData=${rowData}, rowMap=${rowMap}`);
-
-          return (
-            <View
-              style={{
-                borderWidth: 1,
-                borderColor: 'yellow',
-                flexDirection: 'row',
-                alignItems: 'center',
-                backgroundColor: isActive ? 'red' : track.backgroundColor,
-                // alignItems: 'center',
-                justifyContent: 'space-between',
-                height: hp(10),
-                opacity: isActive ? 0.85 : 1,
-              }}>
-              <Text
-                style={{
-                  marginVertical: hp(0.5),
-                  width: wp(80),
-                }}>
-                {`(${index + 1}/${tracks.length}) ID=${track.id}, title=${
-                  track.title
-                }, isActive=${isActive}`}
-              </Text>
-              <TouchableOpacity
-                style={{
-                  backgroundColor: 'blue',
-                  height: hp(10),
-                  width: wp(10),
-                }}
-                // onLongPress={() => {
-                //   // setDraggingTrackIndex(index);
-                //   drag();
-                // }}
-                onPress={() => {
-                  drag();
-                  setDraggingTrackIndex(index);
-                }}>
-                {/*<Icon name={'play'} />*/}
-              </TouchableOpacity>
-            </View>
-          );
-        }}
-        // renderHiddenItem={renderHiddenItem}
-        leftOpenValue={wp(10)}
-        rightOpenValue={-wp(30)}
-        // previewRowKey={'0'}
-        // previewOpenValue={-40}
-        // previewOpenDelay={3000}
-        // onRowDidOpen={onRowDidOpen}
-      />
-
-      // <SwipeRow
-      //   // preview
-      //   // directionalDistanceChangeThreshold={80}
-      //   // friction={10}
-      //   // tension={100}
-      //   leftActivationValue={wp(10)}
-      //   leftOpenValue={wp(10)}
-      //   rightOpenValue={-wp(30)}
-      //   rightActivationValue={-wp(10)}>
-      //   <View
-      //     style={{
-      //       backgroundColor: 'blue',
-      //       height: hp(10),
-      //       alignItems: 'center',
-      //     }}>
-      //     <Text>BACKGROUND</Text>
-      //   </View>
-      //   <View
-      //     style={{
-      //       borderWidth: 1,
-      //       borderColor: 'yellow',
-      //       flexDirection: 'row',
-      //       alignItems: 'center',
-      //       backgroundColor: isActive ? 'red' : track.backgroundColor,
-      //       // alignItems: 'center',
-      //       justifyContent: 'space-between',
-      //       height: hp(10),
-      //       opacity: isActive ? 0.85 : 1,
-      //     }}>
-      //     <Text
-      //       style={{
-      //         marginVertical: hp(0.5),
-      //         width: wp(80),
-      //       }}>
-      //       {`(${index + 1}/${tracks.length}) ID=${track.id}, title=${
-      //         track.title
-      //       }, isActive=${isActive}`}
-      //     </Text>
-      //     <TouchableOpacity
-      //       style={{
-      //         backgroundColor: 'blue',
-      //         height: hp(10),
-      //         width: wp(10),
-      //       }}
-      //       // onLongPress={() => {
-      //       //   // setDraggingTrackIndex(index);
-      //       //   drag();
-      //       // }}
-      //       onPress={() => {
-      //         drag();
-      //         setDraggingTrackIndex(index);
-      //       }}>
-      //       {/*<Icon name={'play'} />*/}
-      //     </TouchableOpacity>
-      //   </View>
-      // </SwipeRow>
-    );
-  };
-
-  const renderTrackItem_Method1 = ({ item: track, index, drag, isActive }) => {
-    return (
-      <SwipeRow
-        // preview
-        // directionalDistanceChangeThreshold={80}
-        // friction={10}
-        // tension={100}
-        leftActivationValue={wp(10)}
-        leftOpenValue={wp(10)}
-        rightOpenValue={-wp(30)}
-        rightActivationValue={-wp(10)}>
-        <View
-          style={{
-            backgroundColor: 'blue',
-            height: hp(10),
-            alignItems: 'center',
-          }}>
-          <Text>BACKGROUND</Text>
-        </View>
-        <View
-          style={{
-            borderWidth: 1,
-            borderColor: 'yellow',
-            flexDirection: 'row',
-            alignItems: 'center',
-            backgroundColor: isActive ? 'red' : track.backgroundColor,
-            // alignItems: 'center',
-            justifyContent: 'space-between',
-            height: hp(10),
-            opacity: isActive ? 0.85 : 1,
-          }}>
-          <Text
-            style={{
-              marginVertical: hp(0.5),
-              width: wp(80),
-            }}>
-            {`(${index + 1}/${tracks.length}) ID=${track.id}, title=${
-              track.title
-            }, isActive=${isActive}`}
-          </Text>
-          <TouchableOpacity
-            style={{
-              backgroundColor: 'blue',
-              height: hp(10),
-              width: wp(10),
-            }}
-            // onLongPress={() => {
-            //   // setDraggingTrackIndex(index);
-            //   drag();
-            // }}
-            onPress={() => {
-              drag();
-              setDraggingTrackIndex(index);
-            }}>
-            {/*<Icon name={'play'} />*/}
-          </TouchableOpacity>
-        </View>
-      </SwipeRow>
-    );
-  };
-
-  const renderTrackItem_DraggingPreview = ({
-    item: track,
-    index,
-    drag,
-    isActive,
-  }) => {
-    return (
-      <View
-        style={{
-          borderWidth: 1,
-          borderColor: 'yellow',
-          flexDirection: 'row',
-          alignItems: 'center',
-          backgroundColor: isActive ? 'red' : track.backgroundColor,
-          // alignItems: 'center',
-          justifyContent: 'space-between',
-          height: hp(10),
-          opacity: isActive ? 0.85 : 1,
-        }}>
-        <Text
-          style={{
-            marginVertical: hp(0.5),
-            width: wp(80),
-          }}>
-          {`(${index + 1}/${tracks.length}) ID=${track.id}, title=${
-            track.title
-          }, isActive=${isActive}`}
-        </Text>
-        <TouchableOpacity
-          style={{
-            backgroundColor: 'blue',
-            height: hp(10),
-            width: wp(10),
-          }}
-          // onLongPress={() => {
-          //   // setDraggingTrackIndex(index);
-          //   drag();
-          // }}
-          onPress={() => {
-            drag();
-            setDraggingTrackIndex(index);
-          }}>
-          {/*<Icon name={'play'} />*/}
-        </TouchableOpacity>
-      </View>
-    );
-  };
-
-  const draggableList = () => (
-    <View style={[styles.container, style]}>
-      {/*<Text>{`Playlist List for ${id}`}</Text>*/}
-
-      {/*<Text>{`Track Count: ${tracks.length}`}</Text>*/}
-      {/*{tracks.map((track, index) => (*/}
-      {/*  <Text*/}
-      {/*    key={index}*/}
-      {/*    style={{*/}
-      {/*      color:*/}
-      {/*        musicInfo.currentlyPlaying.track.id === track.id*/}
-      {/*          ? 'white'*/}
-      {/*          : 'black',*/}
-      {/*      backgroundColor:*/}
-      {/*        musicInfo.currentlyPlaying.track.id === track.id*/}
-      {/*          ? 'blue'*/}
-      {/*          : 'lightgrey',*/}
-      {/*      marginVertical: hp(0.5),*/}
-
-      {/*    }}>*/}
-      {/*    {`[${index}] ${*/}
-      {/*      musicInfo.currentlyPlaying.track.id === track.id ? '->' : ''*/}
-      {/*    } ${JSON.stringify(track.title)}`}*/}
-      {/*  </Text>*/}
-      {/*))}*/}
-
-      <DraggableFlatList
-        // onRef={ref => (draggableFlatList.current = ref)}
-        // simultaneousHandlers={[bottomScrollView, draggableFlatList]}
-        simultaneousHandlers={simultaneousHandlers}
-        data={tracks}
-        // dragItemOverflow={false}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={renderTrackItem}
-        // renderItem={renderTrackItem_DraggingPreview}
-        renderPlaceholder={({ item, index }) => (
-          <View style={{ backgroundColor: 'yellow' }}>
-            <Text>{`${index} ${JSON.stringify(item)}`}</Text>
-          </View>
-        )}
-        // onDragBegin={setDraggingTrackIndex}
-        onDragEnd={({ data, from, to }) => {
-          console.log(`[Current-Playlist] drag end, from=${from}, to=${to}`);
-          setTracks(data);
-          setDraggingTrackIndex(null);
-        }}
-        // dragHitSlop={-200}
-        dragHitSlop={
-          // 1 ? { right: -(width * 0.95 - 20) } : { right: -width }
-          draggingTrackIndex === null ? -200 : 0
-        }
-      />
-    </View>
-  );
-
-  // Swipeable list (dragging not possible, can manually rearrange)
-  // Look for the examples: SwipeToDelete, PerRowConfig
-  // return (
-  //   <SwipeListView
-  //     // data={tracks}
-  //     // renderItem={renderItem}
-  //     // renderHiddenItem={renderHiddenItem}
-  //     // leftOpenValue={75}
-  //     // rightOpenValue={-150}
-  //     // previewRowKey={'0'}
-  //     // previewOpenValue={-40}
-  //     // previewOpenDelay={3000}
-  //     // onRowDidOpen={onRowDidOpen}
-  //     data={tracks}
-  //     renderItem={renderItem}
-  //     renderHiddenItem={renderHiddenItem}
-  //     rightOpenValue={width}
-  //     previewRowKey={'0'}
-  //     previewOpenValue={-40}
-  //     previewOpenDelay={3000}
-  //     onSwipeValueChange={onSwipeValueChange}
-  //     useNativeDriver={false}
-  //   />
-  // );
-
   /////////////////////////////// Test render ///////////////////////////////////////
+  const [rowTranslateAnimatedValues, setRowTranslateAnimatedValues] = useState(
+    [],
+  );
+  const [currentActions, setCurrentActions] = useState(null);
   const [listData, setListData] = useState(
     Array(20)
-      .fill('')
+      .fill()
       .map((_, i) => ({ key: `${i}`, text: `item #${i}` })),
   );
 
-  const [currentActions, setCurrentActions] = useState(null);
+  useEffect(() => {
+    const animVals = {};
+    Array(20)
+      .fill('')
+      .forEach((_, i) => {
+        animVals[`${i}`] = new Animated.Value(1);
+      });
+    console.log(`filled animVals, keys=${Object.keys(animVals)?.length}`);
+    setRowTranslateAnimatedValues(animVals);
+  }, []); // add dep: tracks
 
   // console.log(`this.animationIsRunning=${this.animationIsRunning}`);
 
@@ -394,11 +72,13 @@ const Playlist = ({ style, tracks, setTracks }) => {
   const onSwipeValueChange = swipeData => {
     const { key, value } = swipeData;
 
-    // console.log(
-    //   `onSwipeValueChange: key=${JSON.stringify(key)}, value=${JSON.stringify(
-    //     value,
-    //   )}`,
-    // );
+    console.log(
+      `onSwipeValueChange: key=${JSON.stringify(key)}, value=${JSON.stringify(
+        value,
+      )}, rowTranslateAnimatedValues=${JSON.stringify(
+        rowTranslateAnimatedValues,
+      )}`,
+    );
 
     // console.log(`this.animationIsRunning=${this.animationIsRunning}`);
 
@@ -437,7 +117,7 @@ const Playlist = ({ style, tracks, setTracks }) => {
       style={[
         styles.rowFrontContainer,
         {
-          height: rowTranslateAnimatedValues[data.item.key].interpolate({
+          height: rowTranslateAnimatedValues[data.item.key]?.interpolate({
             inputRange: [0, 1],
             outputRange: [0, 50],
           }),
@@ -619,7 +299,7 @@ const Playlist = ({ style, tracks, setTracks }) => {
         //     ? 'red'
         //     : currentAction === 'moving' && 'blue',
       }}>
-      <Text>currentAction: {JSON.stringify(currentActions)}</Text>
+      {/*<Text>currentAction: {JSON.stringify(currentActions)}</Text>*/}
       <SwipeListView
         // disableRightSwipe
         // leftActivationValue={{}}
